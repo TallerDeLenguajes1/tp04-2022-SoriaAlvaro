93ebc776f312d740770ed975b7a416ffb1ccfc9d
Soria Alvaro <alvarosoria.universidad@gmail.com>
2022-04-25 22:43:37 -0300

    avanzando en el punto 3

--- tp04_2.c
+++ tp04_2.c
@@ -2,100 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <stdbool.h>
 
-struct Tarea {
-    int tareaID; //Numerado en ciclo iterativo
+struct Tarea{
+    int tareaID;
     char *descripcion;
-    int duracion; //entre 10-100
+    int duracion; //10 - 100
 };
 
+typedef struct Tarea tar;
 
-bool confirmarTarea();
+struct Nodo
+{
+    tar t;
+    struct Nodo *siguiente;
+};
 
-int main (){
-    int cantTareas;
-    printf("Ingrese la cantidad de tareas que quieres cargar: ");
-    scanf("%d", &cantTareas);
-    getchar();
-
-    //Asignando memoria dinamica a puntero de estructuras
-    struct Tarea **tareasPendientes, **tareasRealizadas;
-    tareasPendientes=(struct Tarea**)malloc(sizeof(struct Tarea*)*cantTareas);
-    tareasRealizadas=(struct Tarea**)malloc(sizeof(struct Tarea*)*cantTareas);
+typedef struct Nodo nod;
 
 
-    srand(time(NULL));
+int main(){
 
-    //Asignando memoria dinamica a buffer de 200 caracteres
-    char *buffer;
-    buffer=(char*)malloc(sizeof(char)*200);
-
-    for(int i=0; i<cantTareas; i++){
-        //Asignando memoria dinamica para estructuras
-        tareasPendientes[i]=(struct Tarea*) malloc(sizeof(struct Tarea));
-        tareasPendientes[i]->tareaID=i+1;
-        printf("\nIngrese la descripcion: %d\n",i+1);
-        fgets(buffer,200,stdin);
-        fflush(stdin);
-        //Asignando memoria dinamica para caracteres
-        tareasPendientes[i]->descripcion=(char*)malloc(sizeof(char)*(strlen(buffer)+1));
-        strcpy(tareasPendientes[i]->descripcion,buffer);
-        tareasPendientes[i]->duracion=rand()%(100-10+1)+10;
-        printf("\n===================================================\n\n");
-    }
-
-    free(buffer);
-
-    printf("\n\tTAREAS PENDIENTES: \n\n===================================================\n");
-    for(int i=0;i<cantTareas;i++){
-        printf("ID %d\n\n", tareasPendientes[i]->tareaID);
-/*         printf("\n"); */
-        puts(tareasPendientes[i]->descripcion);
-/*         printf("\n"); */
-        printf("duración: %d\n", tareasPendientes[i]->duracion);
-        printf("===================================================\n");
-    }
-
-    for(int i=0;i<cantTareas;i++){
-        if(confirmarTarea()){
-            //Asignando memoria dinamica para estructuras cuando el valor sea true
-            tareasRealizadas[i]=(struct Tarea*) malloc(sizeof(struct Tarea));
-            //pasando las tareas relaizadas a la otra estructura
-            tareasRealizadas[i]=tareasPendientes[i];
-        }
-    }
-
-    printf("\n\tTAREAS REALIZADAS: \n\n===================================================\n");
-    for(int i=0;i<cantTareas;i++){
-        printf("ID %d\n\n", tareasRealizadas[i]->tareaID);
-/*         printf("\n"); */
-        puts(tareasRealizadas[i]->descripcion);
-/*         printf("\n"); */
-        printf("duración: %d\n", tareasRealizadas[i]->duracion);
-        printf("===================================================\n");
-    }
-
-    for(int i=0;i<cantTareas;i++){
-        free(tareasPendientes[i]);
-        free(tareasRealizadas[i]);
-    }
-
-    free(tareasPendientes);
-    free(tareasRealizadas);
-
+    nod * start;
     return 0;
 }
 
-bool confirmarTarea(){
-    printf("\tTerminó la tarea?.\n\tEscriba 'y' para confirmar.\n\tCualquier otra letra para denegar.\n\n");
-    char letra;
-    scanf("%c",&letra);
-    getchar();
-    fflush(stdin);
-    if(letra == 'y' || letra=='Y'){
-        return true;
-    }else{
-        return false;
-    }
+
+nod * crearListaVacia(){return NULL;}
+nod * cargaNodo(int iD){
+    nod * nuevoNodo;
+    nuevoNodo=(nod*)malloc(sizeof(nod));
+    nuevoNodo->t.tareaID= iD;
+    nuevoNodo->t.duracion= rand()%(100-10+1)+10;
+    printf("\n\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\t=\n");
+    printf("\t\t\tINGRESE UNA DESCRIPCIÓN: ");
+    char *buffer;
+    buffer=(char*)malloc(sizeof(char)*200);
+    fgets(buffer,200,stdin);
+    nuevoNodo->t.descripcion=(char*)malloc(sizeof(char)*(strlen(buffer)+1));
+    strcpy(nuevoNodo->t.descripcion,buffer);
+    free(buffer);
+    nuevoNodo->siguiente=crearListaVacia();
+    return nuevoNodo;
+}
+
+void insertarNodo(nod ** start, int cantTareas){
+    nod * nuevoNodo;
+    
 }
